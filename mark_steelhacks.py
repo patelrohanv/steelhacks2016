0d4c4d6cd7e6b9e517ad2e9eb724fe53a290b989
MarkJSy <mjs211@pitt.edu>
2016-02-19 22:35:30 -0500

    Starter code

---
+++ mark_steelhacks.py
@@ -0,0 +1,18 @@
+import tweepy
+
+consumer_key = 'FKO8HPqgt7rJ0EYxQ5pGNYnOM'
+consumer_secret = 'Nhv0BOtTvEub1RD8LA5Azk3uZW8qn5PCgpnyD2BnYkfQ0m5mGr'
+access_token = '1979407176-HJlgun5ty2DHwkV7tG6ErQuMNIAHsp66Q6cR5sl'
+access_secret = 'BU9a0YiJl9W4Xhj3H9Qg1Ypvu8Bn7NZCUTcRutr1FKJdH'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token,access_secret)
+
+api = tweepy.API(auth);
+
+user = api.get_user('guccimanesy');
+
+print user.screen_name
+
+
+print api.rate_limit_status();
