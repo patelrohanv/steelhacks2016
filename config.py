003d9384fa671f64ee9d78fe8371c57d9c64f4b0
cskowronski44 <chs171@pitt.edu>
2016-02-20 10:25:55 -0500

    Added files via upload
    To make it work:
    
    In current folder - mkdir called "data"
    in command line include -d and -q, ex: python twitter_stream_download.py -q sick -d data 
    -q search query
    -d data

---
+++ config.py
@@ -0,0 +1,4 @@
+consumer_key = 'FKO8HPqgt7rJ0EYxQ5pGNYnOM' 
+consumer_secret = 'Nhv0BOtTvEub1RD8LA5Azk3uZW8qn5PCgpnyD2BnYkfQ0m5mGr' 
+access_token = '1979407176-HJlgun5ty2DHwkV7tG6ErQuMNIAHsp66Q6cR5sl' 
+access_secret = 'BU9a0YiJl9W4Xhj3H9Qg1Ypvu8Bn7NZCUTcRutr1FKJdH' 
