995a0f465954edf0d3dd8bd4a3e537bdb944a952
MarkJSy <mjs211@pitt.edu>
2016-02-19 22:44:57 -0500

    Prints a rate_limit check

--- mark_steelhacks.py
+++ mark_steelhacks.py
@@ -1,4 +1,6 @@
 import tweepy
+import json
+
 
 consumer_key = 'FKO8HPqgt7rJ0EYxQ5pGNYnOM'
 consumer_secret = 'Nhv0BOtTvEub1RD8LA5Azk3uZW8qn5PCgpnyD2BnYkfQ0m5mGr'
@@ -8,11 +10,10 @@ access_secret = 'BU9a0YiJl9W4Xhj3H9Qg1Ypvu8Bn7NZCUTcRutr1FKJdH'
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token,access_secret)
 
-api = tweepy.API(auth);
-
-user = api.get_user('guccimanesy');
+api = tweepy.API(auth)
+rate_limit = api.rate_limit_status();
+user = api.get_user('guccimanesy')
 
 print user.screen_name
 
-
-print api.rate_limit_status();
+print(json.dumps(rate_limit, indent = 4))
